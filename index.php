ff532a56f568020d4d14c4d90d7da13efcdc18ae
Bivek <bivek_j@yahoo.com>
2016-12-02 18:05:07 -0500

    changes till now.

--- index.php
+++ index.php
@@ -26,11 +26,11 @@
         <div>
             <div class="nx-row clearfix">
                 <div class="nx-col1"><strong>Account name:</strong></div>
-                <div class="nx-col2"><input type="text" placeholder="First Name"/></div>
+                <div class="nx-col2"><input type="text" placeholder="First Name" class="name"/></div>
             </div>
             <div class="nx-row clearfix">
                 <div class="nx-col1">&nbsp;</div>
-                <div class="nx-col2"><input type="text" placeholder="Last Name"/></div>
+                <div class="nx-col2"><input type="text" placeholder="Last Name" class="name"/></div>
             </div>
             <div class="nx-row clearfix">
                 <div><strong>Creating Account with:</strong></div>
@@ -65,7 +65,7 @@
         <div>
             <div class="nx-row clearfix">
                 <div class="nx-col1">Address:</div>
-                <div class="nx-col2"><input type="text" name="street" placeholder="Number and Street" /></div>
+                <div class="nx-col2"><input type="text" name="street" placeholder="Number and Street" class="street"/></div>
             </div>
             <div class="nx-row clearfix">
                 <div class="nx-col1">&nbsp;</div>
@@ -102,21 +102,52 @@
     </div><!--.account-info ends-->
 
     <div class="nx-vehicles">
-        Vehicles In Showroom: <input type="checkbox" name="notifyme"/><label for="notifyme">Notify me when one of my
+		<div class="veh-sroom">
+       <b> Vehicles In Showroom:</b> &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" name="notifyme"/><label for="notifyme">Notify me when one of my
             vehicles in my showroom gets sold</label>
+			</div>
         <p>
             <span class="imgcontainer"><img src="img/vehicles/1.jpg" width="100"/></span>
             <span class="imgcontainer"><img src="img/vehicles/1.jpg" width="100"/></span>
             <span class="imgcontainer"><img src="img/vehicles/1.jpg" width="100"/></span>
             <span class="imgcontainer"><img src="img/vehicles/1.jpg" width="100"/></span>
+			<span class="imgcontainer"><img src="img/vehicles/1.jpg" width="100"/></span>
         </p>
-        <p><input type="button" value="Save Vehicles"/></p>
-        <p>Select Vehicles: 481 of 481 <input type="text" value="Search by Stock#"/>
-            <select name="year">
+        <p><input type="button" value="Save Vehicles"/ class="myButton fullwidth"></p>
+        
+		<div class="select-vehicles cf">
+		<p>
+		<div class="select-vehicles-colone">
+		<span class="veh-span">Select Vehicles: 481 of 481</span>
+		   <select class="model" name="model">
+                <option value="0">Model</option>
+                <option value="200">200</option>
+                <option value="300">300</option>
+                <option value="400">400</option>
+                <option value="a3">A3</option>
+                <option value="a3sedan">A3 Sedan</option>
+            </select>
+		</div><!--.select-vehicles-colone ends-->
+		<div class="select-vehicles-colone">
+		<span>
+		<input class="sb-stock" type="text" value="Search by Stock#"/>
+            <select class="year" name="year">
                 <option value="2016">2016</option>
                 <option value="2015">2015</option>
                 <option value="2014">2014</option>
             </select>
+		</span>	
+			 <select class="trimstyle" name="trimstyle">
+                <option value="0">Trim / Style</option>
+                <option value="convertible">Convertible</option>
+                <option value="wagon">Wagon</option>
+                <option value="sports">Sports</option>
+                <option value="minivan">Mini-van</option>
+                <option value="utility">Utility</option>
+            </select>
+			</div><!--.select-vehicles-colone ends-->
+			
+			<div class="select-vehicles-collast">
             <select name="make">
                 <option value="0">Make</option>
                 <option value="acura">Acura</option>
@@ -125,23 +156,10 @@
                 <option value="bmw">BMW</option>
                 <option value="ferrari">Ferrari</option>
             </select>
-            <select name="model">
-                <option value="0">Model</option>
-                <option value="200">200</option>
-                <option value="300">300</option>
-                <option value="400">400</option>
-                <option value="a3">A3</option>
-                <option value="a3sedan">A3 Sedan</option>
-            </select>
+			        
 
-            <select name="trimstyle">
-                <option value="0">Trim / Style</option>
-                <option value="convertible">Convertible</option>
-                <option value="wagon">Wagon</option>
-                <option value="sports">Sports</option>
-                <option value="minivan">Mini-van</option>
-                <option value="utility">Utility</option>
-            </select>
+			
+
 
             <select name="color">
                 <option value="0">Color</option>
@@ -151,45 +169,57 @@
                 <option value="grey">Grey</option>
                 <option value="blue">Blue</option>
             </select>
+			
+			</div><!--.select-vehicles-collast ends-->
         </p>
-
+		</div><!--.select-vehicles ends-->
+		<div class="3col cf">		
+		<div class="vehicle-3col">
         <div class="veh-container">
             <h3>2016 Honda CR-V</h3>
             <div><img src="img/vehicles/1.jpg" width="150"/>
-                <p>New 2016 Honda CR-V</p>
-                <p><span class="red-link">Loan:</span> $462.92/mo.</p>
-                <p><span class="red-link">Loan: </span> $332.29/mo</p>
-                <p><span class="red-link">Includes:</span> abc abc abc...</p>
-                <p><a href="javascript:;">View Details</a></p>
-                <p><a href="javascript:;">Add to myShowroom</a></p>
+                <p class="veh-details">New,2016 Honda CR-v EX, AWD 5dr EX, AWD, CVT w/OD, Regular Unleaded I-4 2.4 L/144, City 25/31 Highway, Sport Utility, Stock#36191, $28195.0</p>
+                <p><span class="red-link">Loan:</span> <b class="veh-price">$462.92/mo.</b> for 60 mos@ 7.5% APR</p>
+                <p><span class="red-link">Loan: </span> <b class="veh-price">$332.29/mo.</b> for 36 mos@ 12k/yr</p>
+                <p><span class="red-link">Includes:</span>Sales Tax,Title,License,Doc Fee,Admin</p>
+                <p><a class="veh-button" href="javascript:;">View Details</a></p>
+                <p><a class="veh-button" href="javascript:;">Add to myShowroom</a></p>
             </div>
         </div>
+		</div><!--.vehicle-3col ends-->
+		<div class="vehicle-3col veh-mid">
         <div class="veh-container">
             <h3>2016 Honda CR-V</h3>
-            <div><img src="img/vehicles/1.jpg" width="150"/>
-                <p>New 2016 Honda CR-V</p>
-                <p><span class="red-link">Loan:</span> $462.92/mo.</p>
-                <p><span class="red-link">Loan: </span> $332.29/mo</p>
-                <p><span class="red-link">Includes:</span> abc abc abc...</p>
-                <p><a href="javascript:;">View Details</a></p>
-                <p><a href="javascript:;">Add to myShowroom</a></p>
+             <div><img src="img/vehicles/1.jpg" width="150"/>
+                <p class="veh-details">New,2016 Honda CR-v EX, AWD 5dr EX, AWD, CVT w/OD, Regular Unleaded I-4 2.4 L/144, City 25/31 Highway, Sport Utility, Stock#36191, $28195.0</p>
+                <p><span class="red-link">Loan:</span> <b class="veh-price">$462.92/mo.</b> for 60 mos@ 7.5% APR</p>
+                <p><span class="red-link">Loan: </span> <b class="veh-price">$332.29/mo.</b> for 36 mos@ 12k/yr</p>
+                <p><span class="red-link">Includes:</span>Sales Tax,Title,License,Doc Fee,Admin</p>
+                <p><a class="veh-button" href="javascript:;">View Details</a></p>
+                <p><a class="veh-button" href="javascript:;">Add to myShowroom</a></p>
             </div>
         </div>
+		</div><!--.vehicle-3col ends-->
+		<div class="vehicle-3col">
         <div class="veh-container">
             <h3>2016 Honda CR-V</h3>
-            <div><img src="img/vehicles/1.jpg" width="150"/>
-                <p>New 2016 Honda CR-V</p>
-                <p><span class="red-link">Loan:</span> $462.92/mo.</p>
-                <p><span class="red-link">Loan: </span> $332.29/mo</p>
-                <p><span class="red-link">Includes:</span> abc abc abc...</p>
-                <p><a href="javascript:;">View Details</a></p>
-                <p><a href="javascript:;">Add to myShowroom</a></p>
+             <div><img src="img/vehicles/1.jpg" width="150"/>
+                <p class="veh-details">New,2016 Honda CR-v EX, AWD 5dr EX, AWD, CVT w/OD, Regular Unleaded I-4 2.4 L/144, City 25/31 Highway, Sport Utility, Stock#36191, $28195.0</p>
+                <p><span class="red-link">Loan:</span> <b class="veh-price">$462.92/mo.</b> for 60 mos@ 7.5% APR</p>
+                <p><span class="red-link">Loan: </span> <b class="veh-price">$332.29/mo.</b> for 36 mos@ 12k/yr</p>
+                <p><span class="red-link">Includes:</span>Sales Tax,Title,License,Doc Fee,Admin</p>
+                <p><a class="veh-button" href="javascript:;">View Details</a></p>
+                <p><a class="veh-button" href="javascript:;">Add to myShowroom</a></p>
             </div>
         </div>
-
+		</div><!--.vehicle-3col ends-->
+		</div><!--.3col ends-->
+		
+		<div class="veh-next cf">
         <div class="next nx-clear">
             <input type="submit" value="Save" class="nx-next3"/>
         </div><!--.next ends-->
+		</div><!--.veh-next ends-->
     </div><!--.account-info ends-->
 
 
